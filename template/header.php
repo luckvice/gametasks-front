9e33b54b0e0e2e045719cb536f8bfa8ee3545ba6
Luck <lucasmarcelo93@gmail.com>
2018-12-11 01:38:35 -0200

    Rename Tittle

--- template/header.php
+++ template/header.php
@@ -13,7 +13,7 @@
 
     
     
-    <title>3 Col Portfolio - Start Bootstrap Template</title>
+    <title>GameTasks API Tester</title>
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
